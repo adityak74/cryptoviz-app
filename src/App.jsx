fc0e53c6619c63460d43f38d7db5d59e1e648213
Aditya Karnam <akarnam37@gmail.com>
2021-10-23 18:42:39 -0500

    fix: antd design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
+import MainLayout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-      </header>
+      </header> */}
+      <MainLayout />
     </div>
   );
 }
